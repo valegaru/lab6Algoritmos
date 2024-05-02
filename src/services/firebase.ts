e6f9dab2e014cd90ca2d5d4f9fc91f3d59983a68
valegaru <valegaru2003@gmail.com>
2024-05-02 18:15:41 -0500

    firebase con require comentado

---
+++ src/services/firebase.ts
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+//const firebase = require('firebase/app');
+//const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+//const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBYbGMD0aW7X5vbeJ2jSI9oZfm57ltn1QI',
+	authDomain: 'labs-9a0f6.firebaseapp.com',
+	projectId: 'labs-9a0f6',
+	storageBucket: 'labs-9a0f6.appspot.com',
+	messagingSenderId: '909162520072',
+	appId: '1:909162520072:web:6c5a6b47b5d7145472bbbd',
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+//const auth = getAuth(app);
