d6613425fee6b28da658763051cbc2c75ebdb78c
valegaru <valegaru2003@gmail.com>
2024-05-03 17:22:35 -0500

    firebaseworking

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -31,13 +31,17 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-try {
-	const docRef = await addDoc(collection(db, 'users'), {
-		first: 'Ada',
-		last: 'Lovelace',
-		born: 1815,
-	});
-	console.log('Document written with ID: ', docRef.id);
-} catch (error) {
-	console.error('Error adding document: ', error);
-}
+const uploadData = async () => {
+	try {
+		const docRef = await addDoc(collection(db, 'users'), {
+			first: 'Ada',
+			last: 'Lovelace',
+			born: 1815,
+		});
+		console.log('Document written with ID: ', docRef.id);
+	} catch (e) {
+		console.error('Error adding document: ', e);
+	}
+};
+
+uploadData();
